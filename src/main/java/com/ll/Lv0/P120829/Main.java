bf78456f36c3d8eb6962af54b026042fb8889490
udonggi <dongki1882@naver.com>
2023-02-23 10:43:54 +0900

    second question

---
+++ src/main/java/com/ll/Lv0/P120829/Main.java
@@ -0,0 +1,26 @@
+package com.ll.Lv0.P120829;
+
+public class Main {
+    public static void main(String[] args){
+
+    }
+}
+
+class Solution {
+    public int solution(int angle) {
+        int answer = 0;
+        if(0< angle && angle < 90){
+            answer = 1;
+        }
+        else if (angle == 90){
+            answer = 2;
+        }
+        else if (90 < angle && angle <180){
+            answer = 3;
+        }
+        else if (angle == 180){
+            answer = 4;
+        }
+        return answer;
+    }
+}
--- src/test/java/com/ll/Lv0/P120806/SolutionTests.java
+++ src/test/java/com/ll/Lv0/P120806/SolutionTests.java
@@ -4,13 +4,25 @@ import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
+
 public class SolutionTests {
 
     @Test
-    @DisplayName("t1")
+    @DisplayName("7로 3 나누고 1000 곱하여 소수점 자르면")
     void t1(){
         assertThat(new Solution().solution(7,3)).isEqualTo(2333);
+    }
 
+    @Test
+    @DisplayName("3로 2 나누고 1000 곱하여 소수점 자르면")
+    void t2(){
+        assertThat(new Solution().solution(3,2)).isEqualTo(1500);
+    }
+
+    @Test
+    @DisplayName("1로 16 나누고 1000 곱하여 소수점 자르면")
+    void t3(){
+        assertThat(new Solution().solution(1,16)).isEqualTo(62);
     }
 
 }
