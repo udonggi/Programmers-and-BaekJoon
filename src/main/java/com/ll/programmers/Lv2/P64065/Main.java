4bfc5f046feddadf5490e7847d234587f6956130
udonggi <dongki1882@naver.com>
2023-05-12 22:25:20 +0900

    programmers Lv2_64065 시작

---
+++ src/main/java/com/ll/programmers/Lv2/P64065/Main.java
@@ -0,0 +1,15 @@
+package com.ll.programmers.Lv2.P64065;
+
+public class Main {
+    public static void main(String[] args) {
+        String s = "{{2},{2,1},{2,1,3},{2,1,3,4}}";
+        System.out.println(new Solution().solution(s));
+    }
+}
+
+class Solution {
+    public int[] solution(String s) {
+        int[] answer = {};
+        return answer;
+    }
+}
