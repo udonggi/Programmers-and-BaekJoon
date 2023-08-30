ef8fac016d89429b9d1e8c5ea302ef1e53e92196
udonggi <dongki1882@naver.com>
2023-08-30 21:02:09 +0900

    baekjjon s4_9021(stack사용)

--- src/main/java/com/ll/baekjoon/sliver/s4/B9021/Main.java
+++ src/main/java/com/ll/baekjoon/sliver/s4/B9021/Main.java
@@ -3,38 +3,81 @@ package com.ll.baekjoon.sliver.s4.B9021;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.BufferedReader;
+import java.util.Stack;
 
-public class Main {
+//public class Main {
+//    public static void main(String[] args) throws IOException {
+//        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//        StringBuilder sb = new StringBuilder();
+//
+//        int T = Integer.parseInt(br.readLine());
+//
+//        while (T-- > 0) {
+//            sb.append(solve(br.readLine())).append('\n');
+//        }
+//        System.out.println(sb);
+//
+//    }
+//
+//    public static String solve(String s) {
+//
+//        int count = 0;
+//
+//        for (char c : s.toCharArray()) {
+//
+//            // 여는 괄호일 경우 카운트 증가
+//            if (c == '(') {
+//                count++;
+//            } else if (count == 0) {
+//                return "NO";
+//            } else {
+//                count--;
+//            }
+//        }
+//
+//        if (count == 0) {
+//            return "YES";
+//        } else {
+//            return "NO";
+//        }
+//    }
+//}
+
+public class Main { //stack으로 풀기
+
     public static void main(String[] args) throws IOException {
+
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
 
         int T = Integer.parseInt(br.readLine());
 
-        while (T-- > 0) {
+        for(int i = 0; i < T; i++) {
             sb.append(solve(br.readLine())).append('\n');
         }
-        System.out.println(sb);
 
+        System.out.println(sb);
     }
 
     public static String solve(String s) {
 
-        int count = 0;
+        Stack<Character> stack = new Stack<>();
 
-        for (char c : s.toCharArray()) {
+        for (int i = 0; i < s.length(); i++) {
 
-            // 여는 괄호일 경우 카운트 증가
+            char c = s.charAt(i);
+
+            // 여는 괄호일 경우 스택에 넣는다.
             if (c == '(') {
-                count++;
-            } else if (count == 0) {
+                stack.push(c);
+            } else if (stack.empty()) {
                 return "NO";
             } else {
-                count--;
+                stack.pop();
             }
         }
 
-        if (count == 0) {
+        if (stack.empty()) {
             return "YES";
         } else {
             return "NO";
