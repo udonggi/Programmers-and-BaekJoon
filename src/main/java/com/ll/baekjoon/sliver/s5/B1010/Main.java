40c64500045676396879c894e3cb3df1d5d0e374
udonggi <dongki1882@naver.com>
2023-09-23 21:02:59 +0900

    baekjoon s5_1010_재귀

--- src/main/java/com/ll/baekjoon/sliver/s5/B1010/Main.java
+++ src/main/java/com/ll/baekjoon/sliver/s5/B1010/Main.java
@@ -39,3 +39,46 @@ public class Main {
         System.out.println(sb);
     }
 }
+
+//재귀로 풀기
+/*public class Main {
+
+    static int[][] answer = new int[30][30];
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int T = Integer.parseInt(br.readLine());
+
+        StringTokenizer st;
+
+        StringBuilder sb = new StringBuilder();
+        for(int i = 0; i < T; i++) {
+
+            st = new StringTokenizer(br.readLine(), " ");
+
+            int N = Integer.parseInt(st.nextToken());	// N = r
+            int M = Integer.parseInt(st.nextToken());	// M = n
+
+
+            sb.append(combi(M, N)).append('\n');
+        }
+
+        System.out.println(sb);
+
+    }
+
+    static int combi(int n, int r) {
+
+        if(dp[n][r] > 0) {
+            return answer[n][r];
+        }
+
+        if(n == r || r == 0) {
+            return answer[n][r] = 1;
+        }
+
+        return dp[n][r] = combi(n - 1, r - 1) + combi(n - 1, r);
+    }
+}*/
