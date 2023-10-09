44b85b4cce8e0d14389acd300a12fe2f8412749b
udonggi <dongki1882@naver.com>
2023-10-09 20:55:44 +0900

    baekjoon g5_11729

---
+++ src/main/java/com/ll/baekjoon/gold/g5/B11729/Main.java
@@ -0,0 +1,13 @@
+package com.ll.baekjoon.gold.g5.B11729;
+
+import java.util.*;
+import java.io.*;
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+
+
+    }
+}
