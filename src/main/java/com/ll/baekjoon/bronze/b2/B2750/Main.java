357ffa1f1fa5cf40adff1301f50470e9ae95048c
udonggi <dongki1882@naver.com>
2023-10-16 20:26:28 +0900

    baekjoon b2_2750

---
+++ src/main/java/com/ll/baekjoon/bronze/b2/B2750/Main.java
@@ -0,0 +1,24 @@
+package com.ll.baekjoon.bronze.b2.B2750;
+
+import java.util.*;
+import java.io.*;
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        Set<Integer> set = new TreeSet<>();
+
+        int N = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < N; i++) {
+            set.add(Integer.parseInt(br.readLine()));
+        }
+
+        StringBuilder sb = new StringBuilder();
+
+        for (int i : set) {
+            sb.append(i).append("\n");
+        }
+
+        System.out.println(sb);
+    }
+}
