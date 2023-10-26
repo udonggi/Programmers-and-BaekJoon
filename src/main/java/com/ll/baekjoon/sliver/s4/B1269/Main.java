555e6517526f6907949637acb0b9b667faa14219
udonggi <dongki1882@naver.com>
2023-10-26 11:26:34 +0900

    baekjoon s4_1269(불필요한 코드 제거)

--- src/main/java/com/ll/baekjoon/sliver/s4/B1269/Main.java
+++ src/main/java/com/ll/baekjoon/sliver/s4/B1269/Main.java
@@ -13,7 +13,6 @@ public class Main {
         int B = Integer.parseInt(st.nextToken());
 
         Set<Integer> set1 = new HashSet<>();
-        Set<Integer> set2 = new HashSet<>();
 
         st = new StringTokenizer(br.readLine());
         for (int i = 0; i < A; i++) {
