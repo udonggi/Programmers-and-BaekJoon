f63ad0974f397f1f224c54d823afd920ec6e1329
udonggi <dongki1882@naver.com>
2023-10-28 21:52:20 +0900

    baekjoon s2_11053 start

---
+++ src/main/java/com/ll/baekjoon/sliver/s2/B11053/Main.java
@@ -0,0 +1,16 @@
+package com.ll.baekjoon.sliver.s2.B11053;
+
+import java.util.*;
+import java.io.*;
+public class Main {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+
+    }
+}
