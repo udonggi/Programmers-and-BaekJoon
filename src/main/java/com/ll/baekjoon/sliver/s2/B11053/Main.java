d14e1a2a4fa06e6d9dca7d691f3c793e99ec6331
udonggi <dongki1882@naver.com>
2023-10-29 20:11:58 +0900

    baekjoon s2_11053(dp)

--- src/main/java/com/ll/baekjoon/sliver/s2/B11053/Main.java
+++ src/main/java/com/ll/baekjoon/sliver/s2/B11053/Main.java
@@ -9,7 +9,28 @@ public class Main {
 
         int N = Integer.parseInt(br.readLine());
 
+        int[] s = new int[N];
+        int[] dp = new int[N];
+
         StringTokenizer st = new StringTokenizer(br.readLine());
+
+        for (int i = 0; i < N; i++) {
+            s[i] = Integer.parseInt(st.nextToken());
+            dp[i] = 1;
+        }
+
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < i; j++) {
+                if (s[j] < s[i] && dp[i] < dp[j] + 1) {
+                    dp[i] = dp[j] + 1;
+                }
+            }
+        }
+
+        Arrays.sort(dp);
+
+        System.out.println(dp[dp.length - 1]);
+
 
 
     }
