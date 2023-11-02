ce7f4710d666a90539bd6a9e9976458f6bc2ad1a
udonggi <dongki1882@naver.com>
2023-11-02 23:57:13 +0900

    baekjoon s3_15649

--- src/main/java/com/ll/baekjoon/sliver/s3/B15649/Main.java
+++ src/main/java/com/ll/baekjoon/sliver/s3/B15649/Main.java
@@ -5,5 +5,7 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
+        int N = Integer.parseInt(br.readLine());
+
     }
 }
