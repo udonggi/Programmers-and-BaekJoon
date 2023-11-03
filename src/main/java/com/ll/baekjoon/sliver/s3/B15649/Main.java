513ad94bda70f7fca6e42fc9717b5e34cb0a5f2e
udonggi <dongki1882@naver.com>
2023-11-03 21:22:14 +0900

    baekjoon s3_15649(dfs 백트래킹)

--- src/main/java/com/ll/baekjoon/sliver/s3/B15649/Main.java
+++ src/main/java/com/ll/baekjoon/sliver/s3/B15649/Main.java
@@ -2,10 +2,40 @@ package com.ll.baekjoon.sliver.s3.B15649;
 import java.util.*;
 import java.io.*;
 public class Main {
+    static int[] arr;
+    static boolean[] visited;
+    static StringBuilder sb = new StringBuilder();
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        int N = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
 
+        arr = new int[M];
+        visited = new boolean[N];
+        dfs(N, M, 0);
+        System.out.println(sb);
+
+
+    }
+
+    static void dfs(int N, int M, int d) {
+        if (d == M) {
+            for (int i = 0; i < M; i++) {
+                sb.append(arr[i]).append(" ");
+            }
+            sb.append("\n");
+            return;
+        }
+
+        for (int i = 0; i < N; i++) {
+            if (!visited[i]) {
+                visited[i] = true;
+                arr[d] = i + 1;
+                dfs(N, M, d + 1);
+                visited[i] = false;
+            }
+        }
     }
 }
