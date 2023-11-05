b2ca7bf7ac34cb2cfcac77130c3f5c211575f00d
udonggi <dongki1882@naver.com>
2023-11-05 21:55:14 +0900

    baekjoon s4_17219

---
+++ src/main/java/com/ll/baekjoon/sliver/s4/B17219/Main.java
@@ -0,0 +1,12 @@
+package com.ll.baekjoon.sliver.s4.B17219;
+
+
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+
+}
