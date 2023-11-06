4aaeced6903d2dc22ff2e1c7291f44fc6b352e5b
udonggi <dongki1882@naver.com>
2023-11-06 20:17:16 +0900

    baekjoon s4_17219

--- src/main/java/com/ll/baekjoon/sliver/s4/B17219/Main.java
+++ src/main/java/com/ll/baekjoon/sliver/s4/B17219/Main.java
@@ -5,8 +5,33 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
+    static StringTokenizer st;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
+        st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+
+        Map<String, String> map = new HashMap<>();
+
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            map.put(st.nextToken(), st.nextToken());
+        }
+
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < M; i++) {
+            String s = br.readLine();
+            if (map.containsKey(s)) {
+                sb.append(map.get(s)).append("\n");
+            }
+        }
+
+        System.out.println(sb);
+
+    }
 
 }
