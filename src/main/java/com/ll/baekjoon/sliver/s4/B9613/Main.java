da802bd3f35e3ca5aaeb28afcf1e47b49efbe2e6
udonggi <dongki1882@naver.com>
2023-11-24 20:52:22 +0900

    baekjoon s4_9613

---
+++ src/main/java/com/ll/baekjoon/sliver/s4/B9613/Main.java
@@ -0,0 +1,11 @@
+package com.ll.baekjoon.sliver.s4.B9613;
+
+import java.awt.image.DataBufferDouble;
+import java.util.*;
+import java.io.*;
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    }
+}
