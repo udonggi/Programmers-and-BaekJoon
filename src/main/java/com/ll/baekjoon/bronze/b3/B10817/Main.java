472e1248ffb5d0fd596ca2c506da7dbafbcc6f3b
udonggi <dongki1882@naver.com>
2023-11-26 21:18:08 +0900

    baekjoon b3_10817

---
+++ src/main/java/com/ll/baekjoon/bronze/b3/B10817/Main.java
@@ -0,0 +1,23 @@
+package com.ll.baekjoon.bronze.b3.B10817;
+
+import java.nio.Buffer;
+import java.util.*;
+import java.io.*;
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int[] arr = new int[3];
+
+        for (int i = 0; i < 3; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(arr);
+
+        System.out.println(arr[1]);
+
+    }
+}
